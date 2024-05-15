b548915cf0f9d8683c0c15d881ea29aa40de57f2
ShinBae Kong <osoriz@naver.com>
2024-05-16 00:00:44 +0900

    Il2CPP 지원

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Threading.Tasks;
 using System;
 using NBaseSDK;
+using System.Diagnostics;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,8 +14,8 @@ public class GameManager : MonoBehaviour
     public int maxMessages = 30;
     public float messageSpace = 5.0f;
     public string channelId;
-    public NBaseSDK.Chat _nc = NBaseSDK.Chat.GetInstance();
-    private Queue<NBaseSDK.Message> messageQueue = new Queue<NBaseSDK.Message>();
+    public NBaseSDK.Chat _nc;
+    private Queue<string> messageQueue = new Queue<string>();
     public GameObject chatPanel, textObject;
     public TMP_InputField chatBox;
     public Button[] chatButtons;
@@ -24,6 +25,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        _nc = NBaseSDK.Chat.GetInstance();
         Initialize();
 
         foreach (Button button in chatButtons)
@@ -37,8 +39,9 @@ public class GameManager : MonoBehaviour
         HandleInput();
         if (messageQueue.Count > 0)
         {
-            NBaseSDK.Message message = messageQueue.Dequeue();
-            addMessage("[" + message.sender.name + "] " + message.content, Message.MessageType.PlayerMessage);
+            string message = messageQueue.Dequeue();
+            addMessage(message, Message.MessageType.PlayerMessage);
+            messageQueue.Clear();
         }
     }
     void OnDestroy()
@@ -47,20 +50,21 @@ public class GameManager : MonoBehaviour
         _nc.dispatcher.onMessageDeleted -= OnMessageDeleted;
         _nc.dispatcher.onConnected -= OnConnected;
     }
-    public void EnqueueMessage(NBaseSDK.Message message)
+    public void EnqueueMessage(string message)
     {
-        messageQueue.Enqueue(message);
+        messageQueue.Enqueue(message.ToString());
     }
     private void OnMessageReceived(NBaseSDK.Message message)
     {
-        EnqueueMessage(message);
+        EnqueueMessage(message.content);
     }
     private void OnMessageDeleted(NBaseSDK.Message message)
     {
-        EnqueueMessage(message);
+        EnqueueMessage(message.content);
     }
-    private void OnConnected(User user)
+    private void OnConnected(NBaseSDK.User user)
     {
+        EnqueueMessage("OnConnected");
     }
     private async void Initialize()
     {
@@ -68,20 +72,19 @@ public class GameManager : MonoBehaviour
         {
             _nc.setDebug(true);
             _nc.initialize("339c2b1c-d35b-47f2-828d-5f02a130146a", "alpha", "en");
-            _nc.dispatcher.onMessageReceived += OnMessageReceived;
-            _nc.dispatcher.onMessageDeleted += OnMessageDeleted;
-            _nc.dispatcher.onConnected += OnConnected;
+            setListener();
             await Connect();
+
         }
         catch (InvalidOperationException e)
         {
-            Debug.Log("InvalidOperationException: " + e.Message);
+            UnityEngine.Debug.Log("InvalidOperationException: " + e.Message);
             addMessage("[Error] InvalidOperationException : " + e.Message, Message.MessageType.PlayerMessage);
         }
         catch (Exception e)
         {
-            Debug.Log("Error: " + e.Message);
-            addMessage("[Error] Error : " + e.Message, Message.MessageType.PlayerMessage);
+            UnityEngine.Debug.Log("Error: " + e.Message);
+            addMessage("[Error] : " + e.Message, Message.MessageType.PlayerMessage);
         }
 
     }
@@ -90,18 +93,16 @@ public class GameManager : MonoBehaviour
         try
         {
             var user = await _nc.Connect(
-                   userId: "10001",
-                   name: username,
-                   profile: "https://random.imagecdn.app/500/150",
-                   language: "en");
+                  userId: "10001",
+                  name: username,
+                  profile: "https://random.imagecdn.app/500/150",
+                  language: "en");
 
             await _nc.subscribe(channelId);
 
-            // last messages
             Hashtable filter = new Hashtable
             {
-                { "state", true },
-                { "channel_id", channelId }
+                { "deleted", false },
             };
             Hashtable sort = new Hashtable
             {
@@ -112,23 +113,66 @@ public class GameManager : MonoBehaviour
                 { "offset", 0 },
                 { "per_page", 10 },
             };
-            var messages = await _nc.getMessages(filter, sort, option);
-            if (messages != null)
+           
+            var users = await _nc.getUsers(filter, sort, option);
+            if (users != null)
             {
-                foreach (var message in messages)
+                foreach (var item in users.edges)
                 {
-                    addMessage("[" + message.sender.name + "] " + message.content, Message.MessageType.PlayerMessage);
+                    addMessage("[Users] id" + item.Node.id.ToString(), Message.MessageType.Info);
                 }
             }
+           
+            
+            var channels = await _nc.getChannels(filter, sort, option);
+            if (channels != null)
+            {
+                foreach (var channel in channels.edges)
+                {
+                    addMessage("[Channels] id=" + channel.Node.id.ToString(), Message.MessageType.Info);
+                }
+            }
+
+            var subscriptions = await _nc.getSubscriptions(filter, sort, option);
+            if (subscriptions != null)
+            {
+                foreach (var subscription in subscriptions.edges)
+                {
+                    addMessage("[Subscriptions] id=" + subscription.Node.id.ToString(), Message.MessageType.Info);
+                }
+            }
+            // last messages
+            //Hashtable filter = new Hashtable
+            //{
+            //    { "state", true },
+            //    { "channel_id", channelId }
+            //};
+            //Hashtable sort = new Hashtable
+            //{
+            //    { "created_at", -1 },
+            //};
+            //Hashtable option = new Hashtable
+            //{
+            //    { "offset", 0 },
+            //    { "per_page", 10 },
+            //};
+            //var messages = await _nc.getMessages(filter, sort, option);
+            //if (messages != null)
+            //{
+            //    foreach (var message in messages)
+            //    {
+            //        addMessage("[" + message.sender.name + "] " + message.content, Message.MessageType.PlayerMessage);
+            //    }
+            //}
         }
         catch (InvalidOperationException e)
         {
-            Debug.Log("InvalidOperationException: " + e.Message);
+            UnityEngine.Debug.Log("InvalidOperationException: " + e.Message);
             addMessage("[Error] InvalidOperationException : " + e.Message, Message.MessageType.PlayerMessage);
         }
         catch (Exception e)
         {
-            Debug.Log("Error: " + e.Message);
+            UnityEngine.Debug.Log("Error: " + e.Message);
             addMessage("[Error] Error : " + e.Message, Message.MessageType.PlayerMessage);
         }
 
@@ -162,26 +206,27 @@ public class GameManager : MonoBehaviour
         }
         catch (InvalidOperationException e)
         {
-            Debug.Log("InvalidOperationException: " + e.Message);
+            UnityEngine.Debug.Log("InvalidOperationException: " + e.Message);
             addMessage("[Error] InvalidOperationException : " + e.Message, Message.MessageType.PlayerMessage);
         }
         catch (Exception e)
         {
-            Debug.Log("Error: " + e.Message);
+            UnityEngine.Debug.Log("Error: " + e.Message);
             addMessage("[Error] Error : " + e.Message, Message.MessageType.PlayerMessage);
         }
     }
     public void addMessage(string text, Message.MessageType messageType)
     {
+       
         if (string.IsNullOrWhiteSpace(text))
         {
-            Debug.LogError("Attempted to add an empty message to the chat.");
+            UnityEngine.Debug.LogError("Attempted to add an empty message to the chat.");
             return;
         }
 
         if (textObject == null || chatPanel == null)
         {
-            Debug.LogError("Chat UI components not assigned in the inspector!");
+            UnityEngine.Debug.LogError("Chat UI components not assigned in the inspector!");
             return;
         }
 
@@ -214,7 +259,7 @@ public class GameManager : MonoBehaviour
         }
         catch (Exception e)
         {
-            Debug.LogError("Error adding message: " + e.Message);
+            UnityEngine.Debug.LogError("Error adding message: " + e.Message);
             addMessage("[Error] Error adding message : " + e.Message, Message.MessageType.PlayerMessage);
         }
 
@@ -242,55 +287,55 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //public void setListener()
-    //{
-    //    _nc.dispatcher.onConnected += e =>
-    //    {
-    //        addMessage("Connected to server with id: {0} " + e.ToString(), Message.MessageType.Info);
-    //    };
+    public void setListener()
+    {
+        _nc.dispatcher.onConnected += e =>
+        {
+            EnqueueMessage("Connected to server with id: {0} " + e.ToString());
+        };
 
-    //    _nc.dispatcher.onDisconnected += e =>
-    //    {
-    //        addMessage("Disconnect", Message.MessageType.Info);
-    //    };
-    //    _nc.dispatcher.onErrorReceived += e =>
-    //    {
-    //        addMessage("onErrorReceived: " + e.ToString(), Message.MessageType.Info);
-    //    };
+        _nc.dispatcher.onDisconnected += e =>
+        {
+            EnqueueMessage("Disconnect");
+        };
+        _nc.dispatcher.onErrorReceived += e =>
+        {
+            EnqueueMessage("onErrorReceived: " + e.ToString());
+        };
 
-    //    //_nc.dispatcher.onMessageReceived += e =>
-    //    //{
-    //    //    addMessage("onMessageReceived: " + e.ToString(), Message.MessageType.Info);
-    //    //};
-    //    _nc.dispatcher.onMessageDeleted += e =>
-    //    {
-    //        addMessage("onMessageDeleted: " + e.ToString(), Message.MessageType.Info);
-    //    };
-    //    _nc.dispatcher.onMemberAdded += e =>
-    //    {
-    //        addMessage("onMemberAdded: " + e.ToString(), Message.MessageType.Info);
-    //    };
-    //    _nc.dispatcher.onMemberLeft += e =>
-    //    {
-    //        addMessage("nMemberLeft: " + e.ToString(), Message.MessageType.Info);
-    //    };
-    //    _nc.dispatcher.onMemberUpdated += e =>
-    //    {
-    //        addMessage("onMemberUpdated: " + e.ToString(), Message.MessageType.Info);
-    //    };
-    //    _nc.dispatcher.onMemberDeleted += e =>
-    //    {
-    //        addMessage("onMemberDeleted: " + e.ToString(), Message.MessageType.Info);
-    //    };
-    //    _nc.dispatcher.onStartTyping += e =>
-    //    {
-    //        addMessage("onStartTyping: " + e.ToString(), Message.MessageType.Info);
-    //    };
-    //    _nc.dispatcher.onStopTyping += e =>
-    //    {
-    //        addMessage("onStopTyping: " + e.ToString(), Message.MessageType.Info);
-    //    };
-    //}
+        _nc.dispatcher.onMessageReceived += e =>
+        {
+            EnqueueMessage("onMessageReceived: " + e.content.ToString());
+        };
+        _nc.dispatcher.onMessageDeleted += e =>
+        {
+            EnqueueMessage("onMessageDeleted: " + e.ToString());
+        };
+        _nc.dispatcher.onMemberAdded += e =>
+        {
+            EnqueueMessage("onMemberAdded: " + e.ToString());
+        };
+        _nc.dispatcher.onMemberLeft += e =>
+        {
+            EnqueueMessage("nMemberLeft: " + e.ToString());
+        };
+        _nc.dispatcher.onMemberUpdated += e =>
+        {
+            EnqueueMessage("onMemberUpdated: " + e.ToString());
+        };
+        _nc.dispatcher.onMemberDeleted += e =>
+        {
+            EnqueueMessage("onMemberDeleted: " + e.ToString());
+        };
+        _nc.dispatcher.onStartTyping += e =>
+        {
+            EnqueueMessage("onStartTyping: " + e.ToString());
+        };
+        _nc.dispatcher.onStopTyping += e =>
+        {
+            EnqueueMessage("onStopTyping: " + e.ToString());
+        };
+    }
 
     void OnChatButtonClick(Button button)
     {
